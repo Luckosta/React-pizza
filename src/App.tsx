d6b266dcc9a855855dbb9d10196efc3eb0f6cafb
Leonid Safonov <safonov.lenia2017@yandex.ru>
2023-02-10 14:50:06 +0300

    last changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './scss/app.scss';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
-import Cart from './pages/Cart';
-import FullPizzas from './pages/FullPizzas';
 import MainLayout from './layouts/MainLayout';
+import React, { Suspense } from 'react';
 
+const Cart = React.lazy(() => import('./pages/Cart'));
+const FullPizzas = React.lazy(() => import('./pages/FullPizzas'));
 
 
 
@@ -15,9 +16,18 @@ function App(): JSX.Element {
 		<Routes >
 			<Route path='/' element={<MainLayout />}>
 				<Route path='' element={<Home />} />
-				<Route path='pizza/:id' element={<FullPizzas />} />
-				<Route path='cart' element={<Cart />} />
-				<Route path='*' element={<NotFound />} />
+				<Route path='pizza/:id' element={
+					<Suspense fallback={<div className='container'>Идет загрузка... </div>}>
+						<FullPizzas />
+					</Suspense>}
+				/>
+				<Route path='cart' element={
+					<Suspense fallback={<div className='container'>Идет загрузка... </div>}>
+						<Cart />
+					</Suspense>
+				}
+				/>
+				<Route path='*' element={<NotFound />}/>
 			</Route>
 		</Routes>
 	);
